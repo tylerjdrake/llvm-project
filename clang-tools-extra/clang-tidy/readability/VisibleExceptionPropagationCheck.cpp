6b8e11807a61505cd90adb96a1ae0cc211e887e9
Tyler Drake <tyler.drake@protonmail.com>
2019-12-02 17:23:35 -0500

    Simplify and improve 'throwingStmt'

--- clang-tools-extra/clang-tidy/readability/VisibleExceptionPropagationCheck.cpp
+++ clang-tools-extra/clang-tidy/readability/VisibleExceptionPropagationCheck.cpp
@@ -14,73 +14,138 @@ using namespace clang::ast_matchers;
 
 namespace clang {
 
-namespace {
-AST_MATCHER_P(AttributedStmt, hasStmtAttr, attr::Kind, kind) {
-  auto attrs = Node.getAttrs();
-  for (const Attr* attr : attrs) {
-    if (attr->getKind() == kind) {
-      return true;
+  namespace {
+
+    // Match attributed stmt with certain attribute.
+    AST_MATCHER_P(AttributedStmt, hasStmtAttr, attr::Kind, kind) {
+      auto attrs = Node.getAttrs();
+      for (const Attr* attr : attrs) {
+        if (attr->getKind() == kind) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    // Match attributed stmt with certain sub statment.
+    AST_MATCHER_P(AttributedStmt, hasSubStmt, ast_matchers::internal::Matcher<Stmt>, InnerMatcher) {
+      const Stmt *const Else = Node.getSubStmt();
+      return (Else != nullptr && InnerMatcher.matches(*Else, Finder, Builder));
+    }
+
+    // Helper for throwingExpr.
+    auto hasThrowingFunctionDecl() {
+      return hasDeclaration(
+        functionDecl(
+          unless(
+            anyOf(
+              isNoThrow(),
+              isExternC()))));
+    }
+
+    // Match expressions that can throw.
+    auto throwingExpr() {
+      return expr(
+        anyOf(
+          cxxConstructExpr(hasThrowingFunctionDecl()),
+          callExpr(hasThrowingFunctionDecl())));
+    }
+    auto throwingExprSelfOrDescendant() {
+      return expr(
+        anyOf(
+          throwingExpr(),
+          hasDescendant(throwingExpr())));
+    }
+
+    // Match decl's and stmt's that are annotated.
+    auto markedDecl() {
+      return decl(
+        hasAttr(attr::Kind::MaybeUnhandled));
+    }
+    auto markedStmt() {
+      return stmt(hasParent(attributedStmt(
+        hasStmtAttr(attr::Kind::MaybeUnhandled))));
+    }
+
+    // Match decl's that have a throwing subexpression.
+    auto throwingDecl() {
+      return varDecl(
+        hasDescendant(throwingExpr()));
+    }
+
+    auto throwingIfStmt()
+    {
+      return stmt(
+        ifStmt(
+          hasCondition(
+            throwingExprSelfOrDescendant())));
+    }
+    auto throwingForStmt()
+    {
+      return stmt(
+        forStmt(
+          anyOf(
+            hasLoopInit(
+              throwingExprSelfOrDescendant()),
+            hasCondition(
+              throwingExprSelfOrDescendant()),
+            hasIncrement(
+              throwingExprSelfOrDescendant()))));
+    }
+    auto throwingWhileStmt()
+    {
+      return stmt(
+        whileStmt(
+          hasCondition(
+            throwingExprSelfOrDescendant())));
+    }
+    auto throwingSwitchStmt()
+    {
+      return stmt(
+        switchStmt(
+          hasCondition(
+            throwingExprSelfOrDescendant())));
+    }
+    auto throwingDoStmt()
+    {
+      return stmt(
+        doStmt(
+          hasCondition(
+            throwingExprSelfOrDescendant())));
+    }
+    auto throwingRegularStmt()
+    {
+      return stmt(
+        allOf(
+          unless(ifStmt()),
+          unless(forStmt()),
+          unless(whileStmt()),
+          unless(switchStmt()),
+          unless(doStmt()),
+          throwingExprSelfOrDescendant()));
+    }
+
+    // Match stmt's that have a throwing subexpression...
+    auto throwingStmt() {
+      return stmt(
+        allOf(
+          unless(declStmt()), // matched by "throwing-decl"
+          unless(compoundStmt()), // too course to be useful.
+          unless(cxxThrowExpr()), // explicitly throws already.
+          unless(attributedStmt()), // need only to match attributedStmt substmt.
+          anyOf(
+            hasParent(compoundStmt()), // needs to be an attributable stmt.
+            hasParent(attributedStmt()), // or already attributed.
+            hasParent(caseStmt())),
+          anyOf(
+            throwingIfStmt(),
+            throwingForStmt(),
+            throwingWhileStmt(),
+            throwingSwitchStmt(),
+            throwingDoStmt(),
+            throwingRegularStmt())));
     }
   }
-  return false;
-}
-AST_MATCHER_P(AttributedStmt, hasSubStmt, ast_matchers::internal::Matcher<Stmt>, InnerMatcher) {
-  const Stmt *const Else = Node.getSubStmt();
-  return (Else != nullptr && InnerMatcher.matches(*Else, Finder, Builder));
-}
-auto hasThrowingFunctionDecl() {
-  return hasDeclaration(
-    functionDecl(
-      unless(
-        anyOf(
-          isNoThrow(),
-          isExternC()))));
-}
-auto throwingExpr() {
-  return expr(
-    anyOf(
-      cxxConstructExpr(hasThrowingFunctionDecl()),
-      callExpr(hasThrowingFunctionDecl())));
-}
-auto markedDecl() {
-  return decl(
-    hasAttr(attr::Kind::MaybeUnhandled));
-}
-auto markedStmt() {
-  return attributedStmt(
-    hasStmtAttr(attr::Kind::MaybeUnhandled));
-}
-auto markedIfForWhileSwitchStmt() {
-  return stmt(
-    anyOf(
-      ifStmt(hasParent(markedStmt())),
-      forStmt(hasParent(markedStmt())),
-      whileStmt(hasParent(markedStmt())),
-      switchStmt(hasParent(markedStmt()))));
-}
-auto throwingDecl() {
-  return varDecl(
-    hasDescendant(throwingExpr()));
-}
-auto throwingStmt() {
-  return stmt(
-    allOf(
-      unless(declStmt()), // matched by "throwing-decl"
-      unless(cxxConstructExpr()), // matched by "throwing-decl"
-      unless(compoundStmt()), // too coarse to be useful
-      unless(cxxThrowExpr()), // redundant to annotate throw stmts.
-      unless(hasParent(markedStmt())), // already annotated.
-      unless(hasParent(markedIfForWhileSwitchStmt())), // already annotated
-      unless(hasAncestor( // already annotated 
-        stmt(
-          allOf(
-            unless(compoundStmt()),
-            hasParent(markedIfForWhileSwitchStmt()))))),
-      anyOf(
-        throwingExpr(),
-        hasDescendant(throwingExpr()))));
-}
-}
 
 namespace tidy {
 namespace readability {
